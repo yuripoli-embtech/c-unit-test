fa1f91b1a4c9b6a029a5fe976bd49eaacee5257c
Yuri Embtech <treinne@embtech.com.br>
2025-08-26 15:49:56 -0300

    correcao

--- src/average/average.c
+++ src/average/average.c
@@ -3,6 +3,10 @@
 float average(float arr[], unsigned int size){
     float total = 0;
 
+    if (size == 0) {
+        return 0.0;
+    }
+
     for(unsigned int i = 0; i < size; i++){
         total += arr[i];
     }
